097083048e565a50d88c2ed7d716bafbaa0f853c
makky <75469056+makky-star@users.noreply.github.com>
2020-12-10 20:52:40 +0900

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from FacialRecog import FaceRecog
-names = ['makky','none']
-
-if __name__ == "__main__":
-    face = FaceRecog()
-    #face.capture('image',50)
-    #face.facetrain('image',"train")
-    face.facerecog('train',names)
-    
